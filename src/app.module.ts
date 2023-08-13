e82cb20d46998ac68da8b408d3dc91ef7df8f028
Ahmad Moussawi <aymoussawi@gmail.com>
2023-08-13 10:39:14 +0300

    add auth module + login page

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,11 @@ const routes: Routes = [
     loadChildren: () =>
       import("./pages/about/about.module").then((module) => module.AboutModule),
   },
+  {
+    path: "auth",
+    loadChildren: () =>
+      import("./pages/auth/auth.module").then((module) => module.AuthModule),
+  },
 ];
 
 @NgModule({
---
+++ src/pages/auth/login.component.ts
@@ -0,0 +1,152 @@
+import { Component, ViewChild } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  NgForm
+} from "@angular/forms";
+import { combineLatestWith } from "rxjs/operators";
+
+@Component({
+  selector: "app-login",
+  template: `
+    <form #formRef="ngForm" (ngSubmit)="save(formRef.value)">
+      <pre>{{ formRef.value | json }}</pre>
+      <div class="form-group">
+        <input
+          #firstNameRef="ngModel"
+          name="firstname"
+          required
+          minlength="3"
+          placeholder="First name"
+          [(ngModel)]="firstname"
+          class="form-control"
+        />
+
+        <div *ngIf="$any(firstNameRef.errors)?.required" class="error">
+          The firstname field is required
+        </div>
+
+        <div *ngIf="firstNameRef.errors?.['minlength']" class="error">
+          The firstname should be at least
+          {{ $any(firstNameRef.errors)?.minlength.requiredLength }}, and you
+          typed only
+          {{ $any(firstNameRef.errors)?.minlength.actualLength }} chars.
+        </div>
+      </div>
+
+      <div class="form-group">
+        <input
+          #lastNameRef="ngModel"
+          name="lastname"
+          required
+          minlength="3"
+          placeholder="Last name"
+          [(ngModel)]="lastname"
+          class="form-control"
+        />
+
+        <div *ngIf="$any(lastNameRef.errors)?.required" class="error">
+          The lastname field is required
+        </div>
+        <div *ngIf="$any(lastNameRef.errors)?.minlength" class="error">
+          The lastname should be at least
+          {{ $any(lastNameRef.errors)?.minlength.requiredLength }}, and you
+          typed only
+          {{ $any(lastNameRef.errors)?.minlength.actualLength }} chars.
+        </div>
+      </div>
+
+      <button type="submit">Save</button>
+    </form>
+  `,
+  styles: [
+    `
+      :host {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      form {
+        width: 400px;
+        background: white;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        padding: 1rem;
+      }
+
+      .form-group {
+        margin-bottom: 1rem;
+      }
+
+      .form-control {
+        border: 1px solid #222;
+        padding: 3px;
+        width: 100%;
+      }
+
+      .error {
+        display: none;
+      }
+
+      .ng-touched + .error {
+        display: block;
+        color: red;
+        font-size: 0.7rem;
+        margin-bottom: 1rem;
+      }
+
+      :not(form).ng-invalid.ng-touched {
+        border-left: 4px solid red;
+      }
+
+      :not(form).ng-valid {
+        border-left: 4px solid green;
+      }
+    `,
+  ],
+})
+export class LoginComponent {
+  firstname = "";
+  lastname = "";
+
+  formGroup = new FormGroup({
+    firstName: new FormControl("Ahmad"),
+    lastName: new FormControl("Moussawi"),
+  });
+
+  @ViewChild("formRef") formRef!: NgForm;
+
+  log(obj: any) {
+    console.log(obj);
+  }
+
+  save(data: any) {
+    console.log(data);
+  }
+
+  ngOnInit() {
+    const savedValue = JSON.parse(localStorage.getItem("loginform") ?? "");
+    if (savedValue) {
+      this.firstname = savedValue.firstname;
+      this.lastname = savedValue.lastname;
+    }
+  }
+
+  ngAfterViewInit() {
+    if (
+      this.formRef.valueChanges !== null &&
+      this.formRef.statusChanges !== null
+    ) {
+      this.formRef.statusChanges
+        .pipe(combineLatestWith(this.formRef.valueChanges))
+        .subscribe(([status, value]: any) => {
+          if (status === "VALID") {
+            console.log(value, status);
+          }
+        });
+    }
+  }
+}
